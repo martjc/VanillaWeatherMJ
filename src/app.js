bbf1592949747924d30e5d06c97f5bdbaea3bf86
martjc <123552285+martjc@users.noreply.github.com>
2024-03-26 21:39:28 +0100

    Integrated api w/all weather parameters
    "Day" bug still to be fixed

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@ function updateData(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
 
-  cityElement = document.querySelector("#city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
 
+  let weatherCondition = document.querySelector("#weather-condition");
+  weatherCondition.innerHTML = response.data.condition.description;
+
+  let weatherHumidity = document.querySelector("#humidity");
+  weatherHumidity.innerHTML = response.data.temperature.humidity;
+
+  let weatherWind = document.querySelector("#wind");
+  weatherWind.innerHTML = response.data.wind.speed;
+
+  let date = new Date(response.data.time * 1000);
+  let todayDay = document.querySelector("#day");
+  todayDay.innerHTML = formatDate(date);
+  let nowTime = document.querySelector("#hour");
+  nowTime.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+
+console.log(response);
+
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+    let day = days[date.getDay()];
 }
 
 
@@ -27,3 +49,5 @@ function submitSearch(event) {
 
 let searchFormElement = document.querySelector("#search");
 searchFormElement.addEventListener("submit", submitSearch);
+
+searchCity("Sydney");
