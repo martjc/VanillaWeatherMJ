e717e1ae916ef35efe8041e02414732f2122de37
martjc <123552285+martjc@users.noreply.github.com>
2024-03-26 20:54:07 +0100

    Made temperature interactive

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+function updateData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+let apiKey = "40ab98f05tabc0e10fc0o4394b7f3da7";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(updateData);
+
+}
+
 function submitSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 
