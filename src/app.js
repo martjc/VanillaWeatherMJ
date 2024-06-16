d22aae387b074367a491219658d5851d23749020
martjc <123552285+martjc@users.noreply.github.com>
2024-06-16 19:14:32 +0200

    Integrated search function
    Using API, search function is now working.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector(".weather-temperature");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+ let apiKey = "40ab98f05tabc0e10fc0o4394b7f3da7";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+ axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+
+searchCity("Barcelona");
